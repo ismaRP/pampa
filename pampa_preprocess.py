da259505347aec4765873cb3a95459ac0ccadb85
ismaRP <isma.kosspub@gmail.com>
2025-04-17 12:37:32 +0200

    added preprocessing

---
+++ pampa_preprocess.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+
+import argparse
+import yaml
+import sys
+import os
+
+# local import
+from src import preprocess
+
+
+def  update_params(new_params, template_params):
+    for proc, params in template_params['params'].items():
+        if proc not in new_params['params']:
+            continue
+        for p, value in params.items():
+            if p in new_params['params'][proc]:
+                template_params['params'][proc][p] = new_params['params'][proc][p]
+    return template_params
+
+
+def main():
+    parser = argparse.ArgumentParser()
+
+    group1 = parser.add_argument_group('\nMandatory arguments')
+    group1.add_argument(
+        "-s", dest="spectra", type=str, required=True,
+        help="Path to spectra files in mzML format"
+    )
+    group1.add_argument('-o', dest="output", type=str, required=True,
+        help="Path to output file. Output format is a mzML file"
+    )
+
+    group2 = parser.add_argument_group("\nPreprocessing parameters")
+    group2.add_argument('-p', '--paramfile', dest="param_file", type=str, required=False,
+        help="Path yo YAML with parameters for preprocessing"
+    )
+    group2.add_argument('-r', dest="repl_to_sample", type=str, required=False,
+        help="Table in csv mapping replicate spectra names/IDs to sample name for rpelicate merging."
+             "If not given, it is inferred from the spectrum IDs (abc_1, abc_2, abc_3 -> abc)"
+    )
+
+    group3 = parser.add_argument_group("\nPlotting arguments")
+    group3.add_argument("-q", "--plotnpeaks", dest="make_plot", required=False, action='store_true',
+        help="Whether to plot number of detected peaks per spectrum. It is stored in the sme directory as the output."
+             "It is useful to assess assess if the signal-to-noise threshold is reasonable."
+    )
+
+    args = parser.parse_args()
+
+    default_file = os.path.dirname(sys.argv[0]) + '/preprocessing_params.yml'
+    with open(default_file, 'r') as p:
+        prep_params = yaml.safe_load(p)
+    if args.param_file:
+        with open(args.param_file, 'r') as p:
+            user_params = yaml.safe_load(p)
+        prep_params = update_params(user_params, prep_params)
+
+    preprocess.main(args.spectra, args.output, prep_params, args.repl_to_sample, args.make_plot)
+
+if __name__ == "__main__":
+    main()
---
+++ src/preprocess.py
@@ -0,0 +1,176 @@
+
+import os
+import pyopenms as oms
+import numpy as np
+import matplotlib.pyplot as plt
+from itertools import product
+import pandas as pd
+import textwrap
+import seaborn as sns
+import itertools as it
+
+# local import
+from src import message
+
+def set_filter_parameters(filter, params):
+    param = oms.Param()
+    for key, value in params.items():
+        param.setValue(key, value)
+    filter.setParameters(param)
+
+
+def fix_peaks_spectrum(peaks_spec, profile_spec):
+    mz_peaks, _ = peaks_spec.get_peaks()
+    _, int_prof = profile_spec.get_peaks()
+    peak_indices = [profile_spec.findHighestInWindow(m, 0.1, 0.1) for m in mz_peaks]
+    int_peaks = int_prof[peak_indices]
+    peaks_spec.set_peaks((mz_peaks, int_peaks))
+    return peaks_spec
+
+
+def fix_peaks_int(peaks_exp, profile_exp):
+    assert peaks_exp.getNrSpectra() == profile_exp.getNrSpectra()
+    n = peaks_exp.getNrSpectra()
+    new_spectra = []
+    for i in range(n):
+        prof_spec = profile_exp.getSpectrum(i)
+        peaks_spec = peaks_exp.getSpectrum(i)
+        peaks_spec = fix_peaks_spectrum(peaks_spec, prof_spec)
+        new_spectra.append(peaks_spec)
+    peaks_exp.setSpectra(new_spectra)
+    return peaks_exp
+
+
+class PreprocessingConsumer:
+    def __init__(self, consumer_deiso, params, consumer_iso=None, normalize=False):
+        self._internal_consumer = consumer_deiso
+        self._internal_consumer_iso = consumer_iso
+        self.sg_filter = oms.SavitzkyGolayFilter()
+        self.morph_filter = oms.MorphologicalFilter()
+        self.normalizer = oms.Normalizer()
+        self.peak_picker = oms.PeakPickerIterative()
+        self.normalize = normalize
+        # Set parameters for each filter based on params dictionary
+        for f, par in params.items():
+            if f == 'SGF':
+                set_filter_parameters(self.sg_filter, par)
+            if f == 'BC':
+                set_filter_parameters(self.morph_filter, par)
+            if f == 'PP':
+                set_filter_parameters(self.peak_picker, par)
+            if f == 'NORM':
+                set_filter_parameters(self.normalizer, par)
+
+    def setExperimentalSettings(self, s):
+        self._internal_consumer.setExperimentalSettings(s)
+
+    def setExpectedSize(self, a, b):
+        self._internal_consumer.setExpectedSize(a, b)
+
+    def consumeChromatogram(self, c):
+        self._internal_consumer.consumeChromatogram(c)
+
+    def consumeSpectrum(self, s):
+        # Apply the Savitzky-Golay filter
+        self.sg_filter.filter(s)
+        # Apply the morphological filter
+        self.morph_filter.filter(s)
+        # Apply peak picking
+        picked_spectrum = oms.MSSpectrum()
+        self.peak_picker.pick(s, picked_spectrum)
+        picked_spectrum = fix_peaks_spectrum(picked_spectrum, s)
+        if self._internal_consumer_iso is not None:
+            self._internal_consumer_iso.consumeSpectrum(picked_spectrum)
+        oms.Deisotoper.deisotopeAndSingleCharge(
+            spectra=picked_spectrum,
+            fragment_tolerance=50 , fragment_unit_ppm=True,
+            min_charge=1, max_charge=1, keep_only_deisotoped=True,
+            min_isopeaks=3 , max_isopeaks=6, make_single_charged=False,
+            annotate_charge=False, annotate_iso_peak_count=False,
+            use_decreasing_model=True, start_intensity_check=2 , add_up_intensity=False
+        )
+        if self.normalize:
+            self.normalizer.filterPeakSpectrum(picked_spectrum)
+        # Pass the processed spectrum to the internal consumer
+        self._internal_consumer.consumeSpectrum(picked_spectrum)
+
+
+def to_peaklist(spectra, prep_params, output_dir, make_plot=False):
+    deiso_writer = oms.PlainMSDataWritingConsumer(output_dir + '/proc_deisotoped.mzML')
+    nodeiso_writer = None
+    if make_plot:
+        nodeiso_writer = oms.PlainMSDataWritingConsumer(output_dir + '/proc_nodeisotoped.mzML')
+    deiso_consumer = PreprocessingConsumer(
+        deiso_writer, prep_params['params'], nodeiso_writer, normalize=False)
+
+    if os.path.isfile(spectra):
+        spectra_list = [spectra]
+    elif os.path.isdir(spectra):
+        spectra_list = [os.path.join(spectra, f) for f in os.listdir(spectra) if f.endswith('mzML')]
+    for m in spectra_list:
+        oms.MzMLFile().transform(m, deiso_consumer)
+
+    del deiso_writer, nodeiso_writer, deiso_consumer
+
+    deiso_exp = oms.MSExperiment()
+    oms.MzMLFile().load(output_dir + '/proc_deisotoped.mzML', deiso_exp)
+    deiso_exp.clearMetaDataArrays()
+    if make_plot:
+        nodeiso_exp = oms.MSExperiment()
+        oms.MzMLFile().load(output_dir + '/proc_nodeisotoped.mzML', nodeiso_exp)
+        nodeiso_exp.clearMetaDataArrays()
+    else:
+        nodeiso_exp = None
+    return deiso_exp, nodeiso_exp
+
+
+def abline(slope, intercept, color, ax=None, label=None):
+    if ax is None:
+        ax = plt.gca()
+    x_vals = np.array(ax.get_xlim())
+    y_vals = intercept + slope * x_vals
+    ax.plot(x_vals, y_vals, '--', color=color, label=label)
+
+
+def plot_npeaks(deiso_exp, nodeiso_exp, output_dir):
+    npeaks_deiso = []
+    npeaks_nodeiso = []
+    for deiso_s, nodeiso_s in zip(deiso_exp.getSpectra(), nodeiso_exp.getSpectra()):
+        npeaks_deiso.append(deiso_s.size())
+        npeaks_nodeiso.append(nodeiso_s.size())
+    fig, ax = plt.subplots(
+        nrows=1, ncols=1, figsize=(6, 4), layout='constrained')
+    ax.scatter(npeaks_nodeiso, npeaks_deiso, alpha=0.7)
+    abline(1 / 5, 0, '#1f77b4', ax=ax, label='5 to 1')
+    abline(1 / 6, 0, '#ff7f0e', ax=ax, label='6 to 1')
+    ax.set_xlabel('# of peaks')
+    ax.set_ylabel('# of monoisotopic peaks')
+    ax.legend(loc='best')
+    fig.savefig(output_dir + '/npeaks.png')
+
+def main(spectra, output, prep_params, repl_to_sample, make_plot):
+
+    output_dir, output_file = os.path.split(output)
+    if len(output_dir)>0:
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+    else:
+        output_dir = os.getcwd()
+    print(output_file, output_dir)
+    message.configure(output_dir)
+    print(prep_params)
+    deiso_exp, nodeiso_exp = to_peaklist(spectra, prep_params, output_dir, make_plot)
+
+    if make_plot:
+        plot_npeaks(deiso_exp, nodeiso_exp, output_dir)
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
+    
+
